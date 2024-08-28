666e7a2c2d096700675a184721dd905318a3639e
ShivaniAnandan <shivanianandhan28@gmail.com>
2024-08-28 09:35:09 +0530

    1

--- Database/config.js
+++ Database/config.js
@@ -1,15 +1,24 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
 
-const mongoDBConnectionString = process.env.mongoDBConnectionString
+dotenv.config();
+
+const mongoDBConnectionString = process.env.MONGO_DB_CONNECTION_STRING;
+
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(mongoDBConnectionString)
+        if (!mongoDBConnectionString) {
+            throw new Error("MongoDB connection string is undefined.");
+        }
+
+        const connection = await mongoose.connect(mongoDBConnectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connected to DB");
-    } catch(error){
-        console.log("Error", error);
+    } catch (error) {
+        console.error("Error connecting to DB:", error.message || error);
     }
-}
+};
 
 export default connectDB;
